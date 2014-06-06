b5809c8b94f97c3d1e98d8f1827ada7fe232dfa8
Everett Kropf <ehkropf@gmail.com>
2014-06-06 18:44:57 +0100

    New objects looking better. What has been forgot?

--- rstmap.m
+++ rstmap.m
@@ -9,7 +9,7 @@ properties
 end
 
 methods
-  function F = rstmap(C)
+  function F = rstmap(C, genrect, opts)
     % Here we need to find a rectangle with horizontal slits, and the slit tip
     % preimages on a given circle domain.
     % INPUTS:
@@ -18,12 +18,101 @@ methods
     %   genrect = List of vertices on C_1 in the circle domain that define the
     %   generalized rectangle.
     
-    % Instantiate objective function.
-    % Instantiate numerical solver.
+    if nargin == 0
+      args = {};
+    else
+      if ~isa(C, 'circdomain')
+        error('First argument must be a circle domain.')
+      end
+      if ~isequal(size(genrect(:)), [4, 1])
+        error('Second argument is a 4 element vector.')
+      end
+      if nargin < 3 || isempty(opts)
+        opts = intmapopts;
+      end
+      
+      m = C.m;
+      
+      % Fake rectangle with slits to give to the solver.
+      Prect = cell(m, 1);
+      Prect{1} = polygon([1i, 0, 1, 1+1i]);
+      for j = 2:m
+        Prect{j} = polygon([1 2]); % Only the angles matter for now.
+      end
+      Prect = intpolys(Prect{:});
+      
+      % Circle domain for generalized rectangle.
+      Cr = cell(m, 1);
+      Cr{1} = {C.c(1), C.r(1), unwrap(C.t(1,genrect))};
+      st = C.t(1,genrect(1)) + [0, pi];
+      for j = 2:m
+        Cr{j} = {C.c(j), C.r(j), st};
+      end
+      Cr = circdomain(Cr{:});
+      
+      % Args for superclass constructor are:
+      %  1. Polygons object (here we'll give a fake, since that's what
+      %    we're solving for).
+      %  2. The discretization N variable (number of reflections).
+      %  3. Circle domain.
+      %  4. Objective function
+      %  5. Nonlinear equation solver.
+      
+      args{1} = Prect;
+      args{2} = opts.N;
+      args{3} = Cr;
+      
+      % Instantiate objective function.
+      args{4} = rstobjfun(Prect, Cr, genrect, opts);
+      
+      % Instantiate numerical solver.
+      args{5} = numcontin(args{4}, C.Xu);
+      args{5}.tolerance = opts.tolerance;
+    end
+     
+    % Superclass constructor calls solver if args is not empty.
+    F = F@mcscmap(args{:});
     
-    % Superclass constructor calls solver.
+    if nargin
+      % Post solver operations.
+    end
+  end
+  
+  function [arc, line, k, j] = integpath(F, z)
+    % Integral path for map evaluations.
     
-    % Post solver operations.
+    % Basically a copy of what's done in intmap.
+    vc = F.P.vc;
+    tkj = F.C.t;
+    c = F.C.c;
+    r = F.C.r;
+    
+    [j, t] = nearestcircle(F, z);
+    
+    % distance (and direction to nearest prevertex)
+    dt = tkj(1:vc(j),j) - t;
+    dt = mod(dt+pi, 2*pi) - pi; % dt in [-pi,pi)
+    [mindt, k] = min(abs(dt));
+    
+    if mindt < 100*eps
+      % no arc needed (z is the prevertex or at the same angle on a
+      % line to the circle center)
+      arc = [];
+      line.lpvn = sum(vc(1:j-1)) + k;
+    else
+      arc.left = tkj(k,j);
+      arc.lpvn = sum(vc(1:j-1)) + k;
+      arc.right = tkj(k,j) - dt(k);
+      line.lpvn = 0;
+    end
+    
+    if (j > 1 && (abs(z-c(j)) < r(j) + 100*eps(r(j)))) ...
+        || (j == 1 && abs(z-c(j)) > r(j) + 100*eps(r(j)))
+      % call it on the circle
+      line = [];
+    else
+      line.left = c(j) + r(j)*exp(1i*t);
+    end
   end
 end
 
--- rstobjfun.m
+++ rstobjfun.m
@@ -5,20 +5,96 @@
 classdef rstobjfun < objectivefun
 
 properties
-  PR
-  C
-  quad
+  Prect_
+  Cr_
+  gencirc_
+  N_
+  quad_
 end
 
 methods
-  function F = rstobjfun(C)
+  function F = rstobjfun(Prect, Cr, gencirc, opts)
+    if nargin
+      args{1} = Prect;
+      
+      if nargin < 4 && isempty(opts)
+        opts = intmapopts;
+      end
+      args{2:3} = {opts.monitor, opts.fignum};
+    else
+      args = {};
+    end
+    F = F@objectivefun(args{:});
     
+    if nargin
+      % Save data to self.
+      F.Prect_ = Prect;
+      F.Cr_ = Cr;
+      F.gencirc_ = gencirc;
+      F.N_ = opts.N;
+      
+      % Create quadrature object.
+      fprime = opts.fpclassh(Prect, Cr, opts.N);
+      F.quad_ = gjquad(Prect, fprime, opts.ngj, opts.halfrule);
+    end
+  end
+  
+  function Xc = constrain(F, Xu)
+    % Convert unconstrained circle unknowns back to human levels.
+    
+    m = F.Cr_.m;
+    Xc = zeros(2*(m-1), 1);
+    for j = 1:(m-1)
+      Xu(2*j-1) = mod(Xc(2*j-1), 2*pi);
+      Xu(2*j) = Xc(2*j-1) + 2*pi/(1 + exp(Xc(2*j)));
+    end
+    
+    if any(isinf(Xu))
+      warning('mcsc:unconstr', ...
+        'At least one of the unconstrained variables is at infinity.')
+    end
   end
   
   function f = fun_eval(F, Xu)
     % Calculate objective function.
+    C = F.Cr_;
+    c = C.c;
+    r = C.r;
+    tkj = C.t;
+    [m, ~, vl] = polydat(F.Prect_);
+    quad = F.quad_;
     
+    % Convert unconstrained and store in C.
+    Xc = unconstrain(F, Xu);
+    for j = 1:(m-1)
+      C.t(j+1,1:2) = Xc(2*j-1:2*j);
+    end
     
+    % Compute integration constant.
+    A = arcq(quad, tkj(1,1), 1, tkj(2,1), 2, 0, 1);
+    A = (vl(2,1) - vl(1,1))/A;
+    
+    % Integrate along both sides of inner slits.
+    SL = nan(2, m-1);
+    for j = 1:m-1
+      off = 4 + 2*j;
+      lpvn = off + (1:2)';
+      left = tkj(1:2,j+1);
+      rpvn = flipud(lpvn);
+      right = tkj(2:1,j+1);
+      right(left > right) = right(left > right) + 2*pi;
+      cv(1:2) = c(j+1);
+      rv(1:2) = r(j+1);
+      
+      SL(1:2,j) = A*arcq(quad, left,lpvn, right,rpvn, cv,rv);    
+    end
+    
+    % Components of objective function.
+    f = zeros(2*(m-1), 1);
+    for j = 1:m-1
+      f(2*j-1) = sin(angle(SL(1,j+1)));
+      f(2*j) = abs(SL(1,j+1)) - abs(SL(2,j+1));
+    end
   end
   
   function plot(F)
@@ -33,6 +109,24 @@ methods
       out = fun_eval(F, S.subs{:});
     else
       out = builtin('subsref', F, S);
+    end
+  end
+  
+  function Xu = unconstrain(F, C)
+    % Give unconstrained list of unknown C variables (the interior slit tip
+    % pre-images).
+    
+    if nargin < 2
+      C = F.Cr_;
+    end
+    m = C.m;
+    t = C.t;
+    Xu = zeros(2*(m-1), 1);
+    for j = 1:(m-1)
+      phi = diff([t(j+1,1:2); t(j+1,1) + 2*pi]);
+      phi = mod(phi, 2*pi); % enforce sum(phi) = 2*pi
+      Xu(2*j-1) = t(j+1,1);
+      Xu(2*j) = log(phi(2)/phi(1));
     end
   end
 end
